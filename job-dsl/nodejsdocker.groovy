62a127fc3cb7e1f0f90a284c34fb8dc705e9aee9
Vijay <43067501+VijaySwaroop@users.noreply.github.com>
2019-04-02 11:38:45 +0530

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('vijayar0795/vijay_docker')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
